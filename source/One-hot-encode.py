5b6af6da773fd7eefe4bb4125b613bd641f4649b
MrNiro <425060468@qq.com>
2019-07-23 19:59:56 +0800

    Niro

--- source/One-hot-encode.py
+++ source/One-hot-encode.py
@@ -4,7 +4,7 @@ import csv
 
 def One_Hot_encoder(filename):
     df = pd.read_csv(filename)
-    data1= df["room_type"]
+    data1 = df["room_type"]
     ans = LabelBinarizer().fit_transform(data1)
     return ans
     # ans ["Entire home/apt","Private room", "Shared room"]
@@ -21,22 +21,23 @@ def add_features(filename):
     Entire_home = pd.DataFrame(Entire_home)
     Private_room = pd.DataFrame(Private_room)
     Shared_room = pd.DataFrame(Shared_room)
-    df = pd.concat([Entire_home,Private_room,Shared_room],axis=1)
+    df = pd.concat([Entire_home, Private_room, Shared_room], axis=1)
     return df
 
 def add_to_main(filename):
     original_df = pd.read_csv(filename)
     df = add_features(filename)
-    ent_r = df.iloc[:,0]
-    pri_r = df.iloc[:,1]
-    shr_r = df.iloc[:,2]
+    ent_r = df.iloc[:, 0]
+    pri_r = df.iloc[:, 1]
+    shr_r = df.iloc[:, 2]
     original_df["Entire_home"] = ent_r
     original_df["Private_room"] = pri_r
     original_df["Shared_room"] = shr_r
     original_df.to_csv(filename)
 
 
-print(add_to_main("../data/housing_all.csv"))
+if __name__ == '__main__':
+    print(add_to_main("../data/housing_all.csv"))
 
 
 
