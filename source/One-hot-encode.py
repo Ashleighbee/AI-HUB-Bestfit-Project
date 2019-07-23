fd38a9d0f4a2960b91f4c036bd4ac17f81f1485d
ashleighbee <lanbi@buffalo.edu>
2019-07-23 20:08:46 +0800

    Delete listings that has unrecognizable features

---
+++ source/One-hot-encode.py
@@ -0,0 +1,45 @@
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder,LabelBinarizer
+import pandas as pd
+import csv
+
+def One_Hot_encoder(filename):
+    df = pd.read_csv(filename)
+    data1= df["room_type"]
+    ans = LabelBinarizer().fit_transform(data1)
+    return ans
+    # ans ["Entire home/apt","Private room", "Shared room"]
+
+def add_features(filename):
+    lis = One_Hot_encoder(filename)
+    Entire_home = []
+    Private_room = []
+    Shared_room = []
+    for ele in lis:
+        Entire_home.append(ele[0])
+        Private_room.append(ele[1])
+        Shared_room.append(ele[2])
+    Entire_home = pd.DataFrame(Entire_home)
+    Private_room = pd.DataFrame(Private_room)
+    Shared_room = pd.DataFrame(Shared_room)
+    df = pd.concat([Entire_home,Private_room,Shared_room],axis=1)
+    return df
+
+def add_to_main(filename):
+    original_df = pd.read_csv(filename)
+    df = add_features(filename)
+    ent_r = df.iloc[:,0]
+    pri_r = df.iloc[:,1]
+    shr_r = df.iloc[:,2]
+    original_df["Entire_home"] = ent_r
+    original_df["Private_room"] = pri_r
+    original_df["Shared_room"] = shr_r
+    original_df.to_csv(filename)
+
+
+print(add_to_main("../data/housing_all.csv"))
+
+
+
+
+
+
---
+++ source/write_new.py
@@ -0,0 +1,37 @@
+import csv
+import re
+
+guests = re.compile(r'[0-9]{1,3}')
+f = csv.reader(open('../data/listings-all.csv', encoding='utf-8', errors='ignore'))
+g = list(csv.reader(open('../data/housing_all.csv', encoding='utf-8', errors='ignore')))
+housing_new = open('../data/housing_new.csv', 'a', newline='', encoding='utf-8')
+writer = csv.writer(housing_new, dialect='excel')
+title = ['number', 'id', 'house_ln', 'house_la', 'subway', 'bus_stop', 'street', 'room_type', 'bedroom', 'guests',
+         'num_of_review	review_score', 'Entire_home', '	Private_room', 'Shared_room', '	crime_rate',
+         'accommodates', 'bathroom', 'beds', 'bed_type']
+writer.writerow(title)
+
+
+if __name__ == '__main__':
+    matched = 0
+    count = 0
+    for i in f:
+        if i[0] == 'id':
+            continue
+        line = i
+        match = 0
+        data = []
+        for j in range(matched, len(g)):
+            if line[0] == g[j][1]:
+                match = 1
+                matched = j + 1
+                data = g[j]
+                break
+        if match == 0:
+            continue
+
+        data += [line[53], line[54], line[56], line[57]]
+        writer.writerow(data)
+        count += 1
+        if count % 500 == 0:
+            print(i, 'houses done!')
