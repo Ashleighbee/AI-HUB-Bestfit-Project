6d9a5e6b503bfef07dc008d27e2a2b98aaade452
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2019-07-25 20:46:50 +0800

    [Rename](price_clean.py -> data_clean.py) Reformat codes

--- source/data_clean.py
+++ source/data_clean.py
@@ -1,27 +1,23 @@
 import csv
-import re
 
-judge1 = re.compile(r'[0-9]{1,3}')
-judge2 = re.compile(r'(^[0-9]{1,3})+(.[0-9]{1,6})]?$')
-g = list(csv.reader(open('../data/housing_all.csv', encoding='utf-8', errors='ignore')))
-housing_new = open('../data/housing_new.csv', 'a', newline='', encoding='utf-8')
-writer = csv.writer(housing_new, dialect='excel')
-title = ['id', 'house_ln', 'house_la', 'subway', 'bus_stop', 'park', 'scenery', 'street', 'room_type', 'accommodates',
-         'bathroom', 'bedroom', 'beds', 'bed_type', 'guests', 'num_of_review', 'review_score', 'Entire_home',
-         'Private_room', 'Shared_room', 'crime_rate', 'host_response_time', 'host_response_rate',
-         'superhost', 'host_neighborhood',
-         'daily_price', 'weekly_price', 'monthly_price']
-writer.writerow(title)
+
+def price_clean(line_list: list, writer: csv.writer):
+    for i in range(0, len(line_list)):
+        price = line_list[i][26]
+        if 'daily_price' in price:
+            writer.writerow(line_list[i])
+        elif price == '':
+            continue
+        elif float(price) > 384:
+            continue
+        else:
+            writer.writerow(line_list[i])
+        if i % 500 == 0:
+            print(i, 'houses done!')
 
 
 if __name__ == '__main__':
-    matched = 0
-    for i in range(1, len(g)):
-        if 'f' in g[i][23]:
-            g[i][23] = 0
-        elif 't' in g[i][23]:
-            g[i][23] = 1
-
-        writer.writerow(g[i])
-        if i % 500 == 0:
-            print(i, 'houses done!')
+    housing_new = open('../data/housing_clean.csv', 'a', newline='', encoding='utf-8')
+    csv_writer = csv.writer(housing_new, dialect='excel')
+    g = list(csv.reader(open('../data/housing_all.csv', encoding='utf-8', errors='ignore')))
+    price_clean(g, csv_writer)
