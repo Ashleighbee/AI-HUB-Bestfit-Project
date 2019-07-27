3cad05629e6fc223deed17978110befe8111ca21
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2019-07-28 00:54:35 +0800

    [Modify] Merge response_time into load_data

--- source/load_data.py
+++ source/load_data.py
@@ -434,6 +434,44 @@ def write_crime_rate(filename):
     df2.to_csv("../data/housing_all.csv")
 
 
+def one_hot(filename):
+    df = pd.read_csv(filename)
+    # filename = "../data/housing_all.csv"
+    series1 = df["superhost"]
+    # type(data) is pandas series
+    suph = pd.DataFrame({'superhost':series1})
+    # convert the series I extracted from the housing_all to Dataframe
+    # b/c using get_dummies we have to put in dataFrame
+    onehot = pd.get_dummies(suph)
+    print(onehot)
+
+
+def host_resp(filename):
+    df = pd.read_csv(filename)
+    see = pd.unique(df["host_response_time"])
+    # ['within an hour' 'within a few hours' 'within a day' nan 'a few days or more']
+    serie = df["host_response_time"]
+    lis = []
+    for i in serie:
+        if i == 'within an hour':
+            lis.append(3)
+        elif i == 'within a few hours':
+            lis.append(2)
+        elif i == 'within a day':
+            lis.append(1)
+        else:
+            lis.append(0)
+    host_rt = pd.DataFrame(lis)
+    return host_rt
+
+
+def write_response_time(filename):
+    original_df = pd.read_csv(filename)
+    hostrr = host_resp(filename)
+    original_df["response_time_num"] = hostrr
+    original_df.to_csv(filename)
+
+
 if __name__ == "__main__":
     # write_stations_distance(housefile='../data/housing_all.csv',
     #                         stationfile='../data/bus.csv',
